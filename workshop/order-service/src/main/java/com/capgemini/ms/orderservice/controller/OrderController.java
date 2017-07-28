02c809904addb01fee4fd2405b523522d6e8d7a1
Bartosz Zychal <bartosz.zychal@capgemini.com>
2017-07-28 17:05:37 +0200

    Add Hystrix

--- workshop/order-service/src/main/java/com/capgemini/ms/orderservice/controller/OrderController.java
+++ workshop/order-service/src/main/java/com/capgemini/ms/orderservice/controller/OrderController.java
@@ -1,26 +1,30 @@
 package com.capgemini.ms.orderservice.controller;
 
 
-import com.capgemini.ms.orderservice.clients.CatalogClient;
-import com.capgemini.ms.orderservice.clients.Customer;
-import com.capgemini.ms.orderservice.clients.CustomerClient;
-import com.capgemini.ms.orderservice.clients.Item;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.capgemini.ms.orderservice.clients.CatalogClientHystrix;
+import com.capgemini.ms.orderservice.clients.Customer;
+import com.capgemini.ms.orderservice.clients.CustomerClientHystrix;
+import com.capgemini.ms.orderservice.clients.Item;
 
 @RestController
 public class OrderController {
 
     @Autowired
-    private CustomerClient customerClient;
+    private CustomerClientHystrix customerClient;
 
     @Autowired
-    private CatalogClient catalogClient;
+    private CatalogClientHystrix catalogClient;
 
     @RequestMapping(path = "/customers", method = RequestMethod.GET)
     public ResponseEntity<List<Customer>> getCustomers() {
